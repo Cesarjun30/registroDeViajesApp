7b0bb22493aa2ac255964f291d001c756c9124f6
Cesarjun30 <cesarjun30@gmail.com>
2025-09-29 22:19:51 -0400

    Proyecto web-app registro de viajes con Usando MERN

---
+++ registro_de_ viajes_App/backend/routes/authRoutes.js
@@ -0,0 +1,16 @@
+// routes/authRouter.js
+
+const express = require("express");
+const router = express.Router();
+
+const { registerUser, loginUser } = require("../controllers/authController");
+
+// Ruta para registrar usuario
+
+router.post("/register", registerUser);
+
+// Ruta para login
+
+router.post("/login", loginUser);
+
+module.exports = router;
